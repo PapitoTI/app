926da8f99accf851947297e3dadbde924c06333c
PapitoTI <pedro93546@gmail.com>
2021-08-27 22:57:07 -0300

    Commit tela inicial

--- lib/src/Pages/login/initialPage.dart
+++ lib/src/Pages/login/initialPage.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class InitialPage extends StatefulWidget {
@@ -47,35 +45,103 @@ class _InitialPageState extends State<InitialPage> {
                         color: Colors.white.withOpacity(.18)),
                     alignment: Alignment.center,
                     margin: const EdgeInsets.only(
-                        top: 90, left: 25, bottom: 60, right: 15),
-                    width: 300,
+                        top: 90, left: 15, bottom: 60, right: 15),
+                    width: 330,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 200,
+                            height: 200,
+                            color: Colors.amber,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                        )
+                      ],
+                    ),
                   ),
                   Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         color: Colors.white.withOpacity(.18)),
                     alignment: Alignment.center,
-                    margin:
-                        const EdgeInsets.only(top: 90, bottom: 60, right: 15),
-                    width: 300,
+                    margin: const EdgeInsets.only(
+                        top: 90, left: 15, bottom: 60, right: 15),
+                    width: 330,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 200,
+                            height: 200,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                        )
+                      ],
+                    ),
                   ),
                   Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         color: Colors.white.withOpacity(.18)),
                     alignment: Alignment.center,
-                    margin:
-                        const EdgeInsets.only(top: 90, bottom: 60, right: 15),
-                    width: 300,
+                    margin: const EdgeInsets.only(
+                        top: 90, left: 15, bottom: 60, right: 15),
+                    width: 330,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 200,
+                            height: 200,
+                            color: Colors.purple,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                        )
+                      ],
+                    ),
                   ),
                   Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         color: Colors.white.withOpacity(.18)),
                     alignment: Alignment.center,
-                    margin:
-                        const EdgeInsets.only(top: 90, bottom: 60, right: 15),
-                    width: 300,
+                    margin: const EdgeInsets.only(
+                        top: 90, left: 15, bottom: 60, right: 15),
+                    width: 330,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 200,
+                            height: 200,
+                            color: Colors.pink,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                        )
+                      ],
+                    ),
                   ),
                 ],
               )
