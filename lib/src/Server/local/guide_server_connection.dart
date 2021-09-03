7fa2cfcd5fc8f6da458446557c4ee6c62073a2d6
Felipe <fs.cicatriz@hotmail.com>
2021-09-02 22:54:33 -0300

    local server atualizado

--- lib/src/Server/local/guide_server_connection.dart
+++ lib/src/Server/local/guide_server_connection.dart
@@ -1,65 +1,62 @@
+import 'dart:io';
+
 import 'package:app/src/Models/guide_model.dart';
 import 'package:app/src/Models/itinerary_model.dart';
-import 'package:app/src/Models/itinerary_spot_model.dart';
 import 'package:app/src/Models/schedule_model.dart';
-import 'package:app/src/Models/spot_model.dart';
 
-import '../guide_server_connection_interface.dart';
-
-class GuideServerConnection extends GuideServerConnectionInterface {
-  @override
-  Future<void> createGuide(GuideModel guideModel) async {
-    throw UnsupportedError("");
-  }
-
+class GuideServerConnection {
+  // retornar dados do guia
   @override
   Future<GuideModel> getGuideData() async {
     throw UnsupportedError("");
   }
 
+  // trocar foto de perfil
   @override
-  Future<void> updateGuideData() async {
+  Future<void> updateProfilePicture(File file) async {
     throw UnsupportedError("");
   }
 
+  // trocar senha
   @override
-  Future<void> createSpot(SpotModel spotModel) async {
+  Future<void> updateProfilePassword(
+      String oldPassword, String newPassword) async {
     throw UnsupportedError("");
   }
 
+  // criar roteiro
   @override
   Future<void> createItinerary(ItineraryModel itineraryModel) async {
     throw UnsupportedError("");
   }
 
-  @override
-  Future<ItineraryModel> getItineraryData() async {
-    throw UnsupportedError("");
-  }
-
+  // retornar lista de roteiros do guia
   @override
   Future<List<ItineraryModel>> getGuideItineraries() async {
     throw UnsupportedError("");
   }
 
+  // deletar roteiro do guia
   @override
-  Future<void> createItinerarySpot(
-      ItinerarySpotModel itinerarySpotModel) async {
+  Future<void> deleteItinerary(ItineraryModel itineraryModel) async {
     throw UnsupportedError("");
   }
 
-  @override
-  Future<List<ItinerarySpotModel>> getItinerarySpots() async {
-    throw UnsupportedError("");
-  }
-
+  // retornar lista de agendamentos do guia
   @override
   Future<List<ScheduleModel>> getSchedules() async {
     throw UnsupportedError("");
   }
 
+  // aprovar o agendamento
   @override
-  Future<void> setScheduleApprovation() async {
+  Future<void> approveSchedule(ScheduleModel scheduleModel) async {
+    throw UnsupportedError("");
+  }
+
+  // cancelar o agendamento
+  @override
+  Future<void> cancelSchedule(ScheduleModel scheduleModel) async {
     throw UnsupportedError("");
   }
 }
--- lib/src/Server/local/server_connection_builder.dart
+++ lib/src/Server/local/server_connection_builder.dart
@@ -1,21 +1,15 @@
-import 'package:app/src/Server/guide_server_connection_interface.dart';
 import 'package:app/src/Server/local/guide_server_connection.dart';
 import 'package:app/src/Server/local/tourist_server_connection.dart';
-import 'package:app/src/Server/server_connection_builder_interface.dart';
-import 'package:app/src/Server/tourist_server_connection_interface.dart';
 
 import '../credentials.dart';
 
-class ServerConnectionBuilder extends ServerConnectionBuilderInterface {
-  @override
-  Future<TouristServerConnectionInterface> connectTourist(
+class ServerConnectionBuilder {
+  Future<TouristServerConnection> connectTourist(
       Credentials credentials) async {
     return TouristServerConnection();
   }
 
-  @override
-  Future<GuideServerConnectionInterface> connectGuide(
-      Credentials credentials) async {
+  Future<GuideServerConnection> connectGuide(Credentials credentials) async {
     return GuideServerConnection();
   }
 }
